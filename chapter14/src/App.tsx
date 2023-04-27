87d4645db8c0bca04967ae0e689217df40bdf8ac
jamee <codingjamee@gmail.com>
2023-04-27 10:24:33 +0900

    react를 typescript와 함께 써보기

---
+++ chapter14/src/App.tsx
@@ -0,0 +1,18 @@
+import NewTodo from "./components/NewTodo";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const todos = [{ id: "t1", text: "Finish the course" }];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
+    </div>
+  );
+}
+
+export default App;
---
+++ chapter14/src/components/TodoList.tsx
@@ -0,0 +1,13 @@
+import { link } from "fs"
+
+interface TodoListProps {
+    items: {id: string, text: string}[]
+}
+
+const TodoList : React.FC<TodoListProps>= (props)=>{
+    return <ul>
+        {props.items.map(todo => <li key={todo.id}>{todo.text}</li>)}
+    </ul>
+}
+
+export default TodoList;
