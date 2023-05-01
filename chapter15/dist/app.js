7b20a933b29a12ce973163afc24004b037161156
jamee <codingjamee@gmail.com>
2023-05-01 12:49:20 +0900

    typescript를 사용하여 node 개발하기 초기 환경 세팅

---
+++ chapter15/dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+//현재는 실험적인 구문
+const app = (0, express_1.default)();
+app.listen(3000);
---
+++ chapter15/src/app.ts
@@ -0,0 +1,5 @@
+import express from "express";
+//현재는 실험적인 구문
+
+const app = express();
+app.listen(3000);
